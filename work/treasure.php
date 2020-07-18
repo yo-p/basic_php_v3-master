4c886ba9a2180c8aadd7e6d5bee0fb5a2f7aa3f0
小林　良輝 <papapapipipi42@gmail.com>
2020-07-18 10:11:56 +0900

    Practice PHP

---
+++ work/treasure.php
@@ -0,0 +1,16 @@
+<?php
+$title = '宝探しゲーム -';
+include('_header.php');
+
+?>
+    <section>
+      <img src="img/work2.png" width="400" height="260" alt="宝探しの紹介画像">
+      <h1>宝探しゲーム</h1>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <a href="index.php">もどる</a>
+    </section>
+
+<?php 
+
+include('_footer.php');
+  
